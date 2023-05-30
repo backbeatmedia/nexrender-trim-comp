18e0a06c9334892fee0bf846299c701a8b0b5c16
Jeremy Smith <78971664+backbeatmedia@users.noreply.github.com>
2023-05-30 13:44:03 +0100

    finished & locally tested

--- trimComp.jsx
+++ trimComp.jsx
@@ -12,26 +12,28 @@
     function layerByName(comp, name) {
         for (var i = 1; i <= comp.layers.length; i++) {
             var item = comp.layers[i];
-            if (item instanceof LayerItem && item.name === name) {
+            if (item.name === name)
                 return item;
-            }
         }
     }
 
+    if (typeof _trim !== 'undefined') {
 
-    // get the settings
-    var comp = 'renderComp'; // typeof _trim !== 'undefined' && _trim.get('comp')';
-    var layer = 'target'; // typeof _trim !== 'undefined' && _trim.get('layer');
-    var handle = 0; // typeof _trim !== 'undefined' && _trim.get('handle');
+        // get the settings
+        var comp = _trim.get('comp');
+        var layer = _trim.get('layer');
+        var handle = _trim.get('handle');
 
-    // access the render comp and layer
-    var renderComp = compByName(comp);
-    var referenceLayer = layerByName(renderComp,layer);
+        // access the render comp and layer
+        var renderComp = compByName(comp);
+        var referenceLayer = layerByName(renderComp, layer);
 
-    // calculate the desired outpoint
-    var newOutpoint = referenceLayer.outPoint + handle;
+        // calculate the desired outpoint
+        var newOutpoint = referenceLayer.outPoint + handle;
 
-    // apply to the comp
-    renderComp.workAreaDuration = newOutpoint - renderComp.workAreaStart;
+        // apply to the comp
+        renderComp.workAreaDuration = newOutpoint - renderComp.workAreaStart;
+
+    }
 
 })();
